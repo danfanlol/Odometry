4d15eb1ab77043e5379f5382f04ffbe4d84fc9b3
danfanlol <67649329+danfanlol@users.noreply.github.com>
2022-10-17 16:24:32 -0400

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,51 @@ void on_center_button() {
 		pros::lcd::clear_line(2);
 	}
 }
+pros::Rotation rotation_sensor(12);
+pros::Rotation rotation_sensor2(11);
+pros::Rotation rotation_sensor3(13);
+array<double,3> odometry(double absx, double absy, double robotAngle){
+
+	// odometry uses robot's first starting point as origin.
+	// field will use new coordinate system where the robot facing directly up is theta = 0, robot facing directly down is theta = 180, and robot facing directly left is theta = 270.
+	int rotation1 = rotation_sensor2.get_position();
+	int rotation2 = rotation_sensor.get_position();
+	int rotation3 = rotation_sensor3.get_position();
+
+	double left_arc = rotation1 * (3.141592653589793238462643383279502884197*2.75);
+	double right_arc = rotation2 * (3.141592653589793238462643383279502884197*2.75);
+	double horizontal_arc = rotation3 * (3.141592653589793238462643383279502884197*2.75);
+
+	double tr = 3.25;
+	double tl = 3.25;
+	double tb = 8;
+
+	double angle = (left_arc - right_arc)/(tr + tl);//angle is only change in robot's global angle. We must add the change to the robot's global angle.
+	double difference = angle - robotAngle;
+
+	double x,y;
+	if (left_arc == right_arc){
+		x = horizontal_arc;
+		y = left_arc;
+	}
+	else{
+		y = 2*((right_arc/difference)+tr)*(sin(angle/2));
+		x = 2*((horizontal_arc/difference)+tb)*(sin(angle/2));
+	}
+	double avg = robotAngle + difference/2;
+
+	double r = sqrt(pow(x,2)+pow(y,2));
+	double theta = atan(y/x) + (avg*-1);
+
+	double diffx = r*cos(theta);
+	double diffy = r*sin(theta);
+	std:: array<double,3> array;
+	robotAngle = angle;
+	array[0] = diffx;
+	array[1] = diffy;
+	array[2] = robotAngle;
+	return array;
+}
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -25,20 +70,26 @@ void on_center_button() {
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
+double x,y, robotAngle = 0; // when game starts, robot is at 0,0 and has an angle of 0. This is all relative to its starting positio
 void initialize() {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
 	driveLeftBack.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	driveRightBack.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	driveLeftFront.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	driveRightFront.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
-	lift.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-	intakeLeft.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-	intakeRight.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
-	angler.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+	double curx = x;
+	double cury = y;
+	double curAngle = robotAngle;
+	double counter = 0;
+	while (counter <= 100){
+		std:: array<double,3> pos = odometry(curx,cury,curAngle);
+		curx += pos[0];
+		cury += pos[1];
+		curAngle = pos[2];
+		pros::lcd::set_text(1, to_string(curAngle));
+		counter += 1;
+	}
 
-	pros::ADIGyro gyro('B',0.91);
-	pros::delay(2000);
 
 
 
@@ -81,51 +132,8 @@ void topLeftCorner(){
 	translate(-500,100);
 }
 
-pros::Rotation rotation_sensor(12);
-pros::Rotation rotation_sensor2(11);
-pros::Rotation rotation_sensor3(13);
-array<double,3> odometry(double absx, double absy, double robotAngle){
 
-	// odometry uses robot's first starting point as origin.
-	// field will use new coordinate system where the robot facing directly up is theta = 0, robot facing directly down is theta = 180, and robot facing directly left is theta = 270.
-	int rotation1 = rotation_sensor2.get_position();
-	int rotation2 = rotation_sensor.get_position();
-	int rotation3 = rotation_sensor3.get_position();
 
-	double left_arc = rotation1 * (3.141592653589793238462643383279502884197*2.75);
-	double right_arc = rotation2 * (3.141592653589793238462643383279502884197*2.75);
-	double horizontal_arc = rotation3 * (3.141592653589793238462643383279502884197*2.75);
-
-	double tr = 4.75; //inches for all
-	double tl = 4.75;
-	double tb = 6;
-
-	double angle = (left_arc - right_arc)/(tr + tl);//angle is only change in robot's global angle. We must add the change to the robot's global angle.
-	double difference = angle - robotAngle;
-
-	double x,y;
-	if (left_arc == right_arc){
-		x = horizontal_arc;
-		y = left_arc;
-	}
-	else{
-		y = 2*((right_arc/difference)+tr)*(sin(angle/2));
-		x = 2*((horizontal_arc/difference)+tb)*(sin(angle/2));
-	}
-	double avg = robotAngle + difference/2;
-
-	double r = sqrt(pow(x,2)+pow(y,2));
-	double theta = atan(y/x) + (avg*-1);
-
-	double diffx = r*cos(theta);
-	double diffy = r*sin(theta);
-	std:: array<double,3> array;
-	robotAngle = angle;
-	array[0] = diffx;
-	array[1] = diffy;
-	array[2] = robotAngle;
-	return array;
-}
 array<double,3> goToPosition(double destx, double desty, double curx, double cury, double curAngle){
 	double a = destx - curx;
 	double b = desty- cury;
@@ -192,13 +200,24 @@ array<double,3> goToPosition(double destx, double desty, double curx, double cur
 	array[2] = curAngle;
 	return array;
 }
-double x,y, robotAngle = 0; // when game starts, robot is at 0,0 and has an angle of 0. This is all relative to its starting positio
 void autonomous() {
-	std:: array<double,3> pos = goToPosition(20,20,x,y,robotAngle);
-	x = pos[0];
-	y = pos[1];
-	robotAngle = pos[2];
+	double curx = x;
+	double cury = y;
+	double curAngle = robotAngle;
 
+
+	//std:: array<double,3> pos = goToPosition(10,10,x,y,robotAngle);
+	//x = pos[0];
+	//y = pos[1];
+	//robotAngle = pos[2];
+	while (true){
+		std:: array<double,3> pos = odometry(curx,cury,curAngle);
+		curx += pos[0];
+		cury += pos[1];
+		curAngle = pos[2];
+		pros::lcd::set_text(1, to_string(curAngle));
+
+	}
 }
 
 /**
@@ -215,13 +234,5 @@ void autonomous() {
  * task, not resume it from where it left off.
  */
 void opcontrol() {
-	while (true){
-		setDriveMotors();
-		setIntakeMotors();
-		setLiftMotor();
-		setAnglerMotor();
 
-		pros::delay(10);
-
-	}
 }
